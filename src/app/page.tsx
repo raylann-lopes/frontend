a419d11770abfa153134f1483f1136d68f8d8cff
raylann_lopes <raylanlopes@icloud.com>
2024-11-20 12:33:00 -0300

    feat: add background to page home and colors in globals.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-import Page from "@/components/template/page";
+import Logo from "@/components/template/logo";
+import Link from "next/link";
 
-export default function Home() {
+export default function home() {
   return (
-    <Page>
-      <div>Inicio</div>
-    </Page>
+    <div
+      className="
+       flex h-screen flex-col justify-center  items-center
+       bg-[url('/background-elementos.svg')] bg-cover "
+    >
+      <div>
+        <Logo />
+        <p className="text-zinc-500 font-light w-96 leading-6">
+          Crie e gerencie o convite do seu evento de forma rapida e facil, sem
+          complicacoes!
+        </p>
+      </div>
+      <Link href="/evento">Crie o seu evento!</Link>
+    </div>
   );
 }
